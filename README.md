e47993aca7e81e878feddc75676c669c5903f622
wimos-ai <mosierwilliam6@gmail.com>
2026-01-13 12:54:55 -0600

    Made the code able to be ran on Windows. Did some wite up stuff. Committing now to link in the github files.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # 2025-Cosmic-Motor-Power-Analysis
+
+I did some analysis of the power used while competing at 2025 CoSMiC.
+All data from the `lance_log_data_2025_05_24-13_24_51` rosbag. 
+At this time we were operating on 4S Lipos at ~14-16 VDC
+
+## Methodology
+
+I dumped the data from the rosbag into CSV files stored in `raw`
+The raw data was taken in approximately 0.05 second intervals
+
+For each motor, I calculated an equivalent windowed VRMS value using the Discrete Time Sliding Window calculation method with a window size of 0.1, 1, 3, 5, 10, and 60 seconds. After this is calculated I may or may not drop any zero terms.
+
+For each motor I looked at output / stator current, and supply current to the motor controller. 
+
+## Output Spec
+
+### XLSX Files
+There is one XLSX file per motor. It contains the raw data, then one page of calculated RMS values for each window size at both the input and output current sensor. Each page of calculated RMS values also contains the calculated means with and without zeros included, and calculated Q0, Q1, Q2, Q3, andQ4 points for the data set with the zeros dropped.
+
+### Box Plots
+There are 4 sets of box plots for each motor. 
+
+|                | Zeros Included                        | Zeros Dropped                            |
+|----------------|---------------------------------------|------------------------------------------|
+| Output Current | *.info.csv_output_current_boxplot.png | *.info.csv_output_current_nz_boxplot.png |
+| Input Current  | *.info.csv_input_current_boxplot.png  | *.info.csv_input_current_nz_boxplot.png  |
+
+The Y axis has units of Amps
+The X axis is spaced by Raw Data, then each of the sliding window sizes in order from least to greatest.
--- analyzer.py
+++ analyzer.py
@@ -17,14 +17,7 @@ def read_csv_handle_time(csv_path: str):
     return df
 
 
-def windowed_rms_from_csv(csv_path: str, window_sec: float | int, value_p: str):
-
-    df = read_csv_handle_time(csv_path)
-
-    time = df["time"].to_numpy()
-
-    value = df[value_p].to_numpy()
-
+def generate_windowed_rms(time, values, window_sec) -> list[dict]:
     results = []
 
     start_time = time[0]
@@ -37,7 +30,7 @@ def windowed_rms_from_csv(csv_path: str, window_sec: float | int, value_p: str):
         # Indices within window
         mask = (time >= t0) & (time <= t1)
         t_win = time[mask]
-        v_win = value[mask]
+        v_win = values[mask]
 
         if len(t_win) < 2:
             t0 = t1
@@ -55,27 +48,37 @@ def windowed_rms_from_csv(csv_path: str, window_sec: float | int, value_p: str):
         results.append({"window_start": t0, "window_end": t1, "rms": rms})
 
         t0 = t1
+    return results
+
+
+def windowed_rms_from_csv(csv_path: str, window_sec: float | int, value_p: str):
+
+    df = read_csv_handle_time(csv_path)
+
+    time = df["time"].to_numpy()
+
+    value = df[value_p].to_numpy()
+
+    results = generate_windowed_rms(time, value, window_sec)
 
     def rms_v_itr():
+        """
+        The results dict is an annoying layout for list methods.
+        Make a memory efficient generator to traverse through the RMS values
+        """
         for result in results:
             yield result["rms"]
 
-    nz_rms = list(filter(lambda x: x != 0, rms_v_itr()))
+    nz_rms: list[float | int] = list(filter(lambda x: x != 0, rms_v_itr()))
 
-    avg_nz_rms = sum(nz_rms) / len(nz_rms)
+    results[0]["rms_mean"] = sum(rms_v_itr()) / sum(1 for _ in rms_v_itr())
 
-    results[0]["rms_mean"] = np.mean(nz_rms)
-
-    results[0]["nz_rms_mean"] = avg_nz_rms
-
-    q1 = np.quantile(nz_rms, 0.25)
-    q2 = np.quantile(nz_rms, 0.5)
-    q3 = np.quantile(nz_rms, 0.75)
+    results[0]["nz_rms_mean"] = np.mean(nz_rms)
 
     results[0]["nz_rms_q0"] = min(nz_rms)
-    results[0]["nz_rms_q1"] = q1
-    results[0]["nz_rms_q2"] = q2
-    results[0]["nz_rms_q3"] = q3
+    results[0]["nz_rms_q1"] = np.quantile(nz_rms, 0.25)
+    results[0]["nz_rms_q2"] = np.quantile(nz_rms, 0.5)
+    results[0]["nz_rms_q3"] = np.quantile(nz_rms, 0.75)
     results[0]["nz_rms_q4"] = max(nz_rms)
 
     return pd.DataFrame(results)
@@ -96,7 +99,7 @@ def create_boxplot(
             arr_no_zeros = arr[arr != 0]
             values[idx] = arr_no_zeros
 
-    plt.boxplot(values, showmeans=True)
+    plt.boxplot(values, showmeans=True, showfliers=False, whis=(0, 100))
     ticks = [1]
     labels = ["Raw"]
     for w in w_values:
@@ -111,15 +114,16 @@ def create_boxplot(
 
 def main():
     csv_files = [
-        "hopper_act.info.csv",
-        "hopper_belt.info.csv",
-        "track_left.info.csv",
-        "track_right.info.csv",
-        "trencher.info.csv",
+        os.path.join(os.getcwd(), "raw", "hopper_act.info.csv"),
+        os.path.join(os.getcwd(), "raw", "hopper_belt.info.csv"),
+        os.path.join(os.getcwd(), "raw", "track_left.info.csv"),
+        os.path.join(os.getcwd(), "raw", "track_right.info.csv"),
+        os.path.join(os.getcwd(), "raw", "trencher.info.csv"),
     ]
 
     for csv_file in csv_files:
-        excel_file = f"./out/{csv_file}_analyzed.xlsx"
+        csv_name = os.path.basename(csv_file)
+        excel_file = os.path.join(os.getcwd(), "out", f"{csv_name}_analyzed.xlsx")
         with pd.ExcelWriter(excel_file, engine="xlsxwriter") as writer:
             workbook = writer.book
 
@@ -147,10 +151,17 @@ def main():
                         index=False,
                     )
                 create_boxplot(
-                    f"out/{csv_file}_{value_p}_boxplot.png", values, W_VALUES, value_p
+                    os.path.join(
+                        os.getcwd(), "out", f"{csv_name}_{value_p}_boxplot.png"
+                    ),
+                    values,
+                    W_VALUES,
+                    value_p,
                 )
                 create_boxplot(
-                    f"out/{csv_file}_{value_p}_nz_boxplot.png",
+                    os.path.join(
+                        os.getcwd(), "out", f"{csv_name}_{value_p}_nz_boxplot.png"
+                    ),
                     values,
                     W_VALUES,
                     value_p,
@@ -159,6 +170,7 @@ def main():
 
 
 if __name__ == "__main__":
-    if not os.path.exists("./out"):
-        os.mkdir("./out")
+    output_path = os.path.join(os.getcwd(), "out")
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
     main()
